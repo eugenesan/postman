9ede6f11335d7725554817d8d92272468575095a
Edward Apap <schumifer@hotmail.com>
2012-08-20 00:44:52 +0200

    Updated changelog and version to 0.3.0

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='postman',
-    version='0.2.6',
+    version='0.3.0',
     license='GPL-3',
     author='Edward Apap',
     author_email='schumifer@hotmail.com',
